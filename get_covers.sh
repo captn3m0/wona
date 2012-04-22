27cc8b45a97ac1d80a6a89e9ca0f47540616fba2
Abhay Rana <capt.n3m0@gmail.com>
2012-04-22 21:16:13 +0530

    Initial commit for gh-pages

---
+++ get_covers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for file in *.pdf
+do
+    base=`basename "$file" .pdf`
+    echo "Converting $file"
+    pdftk "$file" cat 1 output /tmp/cover.pdf 2>/dev/null;
+    convert -quality 60% -size 100x142 -resize 100x142 /tmp/cover.pdf $base.jpg;
+done
