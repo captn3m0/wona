961a535f946c1e17d3c56646ded0dd494a4e2920
Nemo <me@captnemo.in>
2020-05-10 01:23:27 +0530

    New issues.

--- get_covers.sh
+++ get_covers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for file in *.pdf
 do
-    base=`basename "$file" .pdf`
-    echo "Converting $file"
-    pdftk "$file" cat 1 output /tmp/cover.pdf 2>/dev/null;
-    convert -quality 60% -size 100x142 -resize 100x142 /tmp/cover.pdf $base.jpg;
+  base=`basename "$file" .pdf`
+  if [[ ! -f "$base.jpg" ]]; then
+      echo "Converting $file"
+      pdftk "$file" cat 1 output /tmp/cover.pdf 2>/dev/null
+      convert -quality 60% -size 100x142 -resize 100x142 /tmp/cover.pdf "$base.jpg"
+    fi
 done
